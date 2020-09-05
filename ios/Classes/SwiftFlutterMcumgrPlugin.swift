ce704639abc9f35c83f0f1d5023a52af39b11cd3
Tomas Koranda <tom.koranda@gmail.com>
2020-09-05 17:17:14 +0200

    add mcumgr-ios lib dependency, change min ios version

--- ios/Classes/SwiftFlutterMcumgrPlugin.swift
+++ ios/Classes/SwiftFlutterMcumgrPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import McuManager
 
 public class SwiftFlutterMcumgrPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
